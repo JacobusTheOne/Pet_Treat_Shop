8d6e70e209376ee4d9cb1161978a7eac8ad3124c
JacobusTheOne <jacobus.jacobs1@yahoo.com>
2022-02-21 15:04:39 +0200

    Add project files.

---
+++ Pet_Treat_Shop/Program.cs
@@ -0,0 +1,31 @@
+﻿using Pet_Treat_Shop;
+using System;
+using static System.Console;
+class Program
+{
+    public static void Main()
+    {
+        Treats treatShop = new Treats();
+        bool quit = false;
+        do
+        {
+            WriteLine("Enter the number of biscuits: ");
+            treatShop.amountOfBiscuits = int.Parse(ReadLine());
+            WriteLine("Enter the number of chewies: ");
+            treatShop.amountOfChewies = int.Parse(ReadLine());
+            WriteLine("Enter the number of vitamins: ");
+            treatShop.amountOfVitamins = int.Parse(ReadLine());
+            WriteLine("Please enter the bucket size [Normal,Large]: ");
+            treatShop.SetBucketSize(ReadLine().ToUpper());
+            WriteLine("Is it the weekend? [Y = yes / N = no]: ");
+            treatShop.SetWeekend(ReadLine().ToUpper());
+            WriteLine(treatShop.CalculateAmount());
+            WriteLine("If want to quit press 1 or enter: ");
+            if (ReadLine() == "1")
+            {
+                quit = true;
+            }
+        } while (quit == false);
+    }
+}
+
---
+++ Pet_Treat_Shop/Treats.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pet_Treat_Shop
+{
+    public class Treats
+    {
+        public int amountOfChewies { get; set; } = 0;
+        public int amountOfVitamins { get; set; } = 0;
+        public int amountOfBiscuits { get; set; } = 0;
+        private float totalOwed { get; set; }
+        public bool large { get; set; } = false;
+        public bool weekend { get; set; } = false;
+        public string CalculateAmount()
+        {
+
+            if (large)
+            {
+                totalOwed = amountOfChewies * 4.50f + amountOfVitamins * 4.15f + amountOfBiscuits * 3.75f;
+            }
+            else
+            {
+                totalOwed = amountOfChewies * 4.10f + amountOfVitamins * 2.50f + amountOfBiscuits * 2.00f;
+            }
+            
+            if(weekend)
+            {
+                totalOwed *= 115f / 100f;
+            }
+            if( (amountOfBiscuits+amountOfChewies+amountOfVitamins) >= 20)
+            {
+                totalOwed *= 80f / 100f;
+            } 
+            if( amountOfChewies >= 10 && large)
+            {
+                totalOwed *= 90f / 100f;
+            } 
+            if( amountOfVitamins >= 7 && !large)
+            {
+                totalOwed *= 95f / 100f;
+            }
+            totalOwed += 2;
+            // return String.Format("{0:C}",totalOwed); this formatting if you want to return in rand.
+            return String.Format("{0}",Math.Round(totalOwed,2));
+        }
+
+        public void SetBucketSize(string bucketSize = "normal")
+        {
+            if (bucketSize.ToUpper() == "LARGE")
+            {
+                large = true;
+            }
+            else
+            {
+                large = false;
+            }
+        }
+
+        public void SetWeekend(string isWeekend = "N")
+        {
+            if (isWeekend == "Y")
+            {
+                weekend = true;
+            }
+            else
+            {
+                weekend = false;
+            }
+        }
+    }
+}
